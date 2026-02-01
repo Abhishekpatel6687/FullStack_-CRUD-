1bf12993a90d3b29a3f609c20adac0471729407f
Abhishek Patel <abhishekpatel6687@gmail.com>
2026-02-01 19:39:24 +0530

    ui design

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,24 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import "./App.css"
-import Header from './components/Header'
-import AddEmp from './components/AddEmp'
-import UpdateEmp from './components/UpdateEmp';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import AddEmp from "./components/AddEmp";
+import UpdateEmp from "./components/UpdateEmp";
 
 const App = () => {
-    // const singleEmp ={  name: "singleEmp.name",
-    // email: "singleEmp.emai",
-    // designation: "ingleEmp.designation",
-    // empid:" singleEmp.empid"}
-    const [singleEmp, setSingleEmp] = useState({})
+  const [singleEmp, setSingleEmp] = useState({});
+
   return (
     <div>
-        <Router>
-         <Routes>
-           <Route path="/" element={<Header setSingleEmp={setSingleEmp} />} />
-           <Route path="/AddEmp" element={<AddEmp />} />
-           <Route path="/update" element={<UpdateEmp  singleEmp={singleEmp}/>} />
-
-           
-         </Routes>
-       </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header setSingleEmp={setSingleEmp} />} />
+          <Route path="/AddEmp" element={<AddEmp />} />
+          <Route path="/update" element={<UpdateEmp singleEmp={singleEmp} />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
